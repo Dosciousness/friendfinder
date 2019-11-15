311153565aa928b34f10b13ada854acc3b422ba0
Sam Boal <sboal510@gmail.com>
2019-11-14 17:11:29 -0800

    final working

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Jason Derulo",
+    photo: "https://i.forbesimg.com/media/lists/people/jason-derulo_416x416.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Joseph Johnson",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Jeremy Lite",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Claire West",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Jill Hisk",
+    photo: "https://cdn.crownmediadev.com/dims4/default/b42d1a6/2147483647/crop/1658x936%2B0%2B226/resize/726x410%5E/quality/80/?url=https%3A%2F%2Fcdn.crownmediadev.com%2Fb2%2Ffb%2F00bf31ad47bca72d6871155e0a01%2Fmystery101-0821-cb-rt.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Frank Glomp",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
